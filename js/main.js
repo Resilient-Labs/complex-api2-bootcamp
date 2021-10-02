2d411b13aa3b50625ffc26655e35c4a17da35951
NDNey <ndavidney@gmail.com>
2021-10-01 23:14:47 -0400

    API Answer

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+
+document.querySelector('button').addEventListener('click',getMovie)
+
+
+function getMovie(){
+
+    let title = document.querySelector('input').value
+    let url = `http://www.omdbapi.com/?apikey=eb729f0b&t=${title}`
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+        if(data.Plot === undefined){
+            alert('Please enter a valid movie name') 
+            return
+        }
+        let urlG = `https://api.giphy.com/v1/gifs/search?api_key=6SZD7JEgPJpoKZbbJAtylZhgwsKfNAsw&q=${data.Title}`
+        document.querySelector('p').innerText = `${data.Title} was Released ${data.Released}: ${data.Plot}`
+        fetch(urlG)
+        .then(res => res.json())
+        .then(dataGif=>{
+            document.querySelector('iframe').src = dataGif.data[Math.floor(Math.random()*50)].embed_url
+        })
+    })
+    .catch(err => alert('Please enter a valid movie name'))
+}
