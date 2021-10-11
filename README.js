f864cd1f7b1be855b96c1b7d6732d14d977da94a
juliedodev <juliedo.dev@gmail.com>
2021-10-11 13:09:23 -0400

    complex-api2-bootcamp

--- README.js
+++ README.js
@@ -1,17 +1,17 @@
 This is an anime quote app that will generate an image and a quote belonging to the anime. 
 
-Link to Project: [https://juliedodev-simpleapi2.netlify.app]
+Link to Project: [https://juliedodev-complexapi2.netlify.app]
 
 
-<img src="simpleapi2.png">
+<img src="complexapi2.png">
 
 How It's Made:
 
 Tech used: 
 <ul>
-<li>HTML<li>
+<li>HTML</li>
 <li>CSS</li>
-<li>JavaScript<li>
+<li>JavaScript</li>
 </ul>
 
 Optimizations:
