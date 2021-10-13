34b52af1262a1442d71adc15a1356814c37e038a
Alexa M <88990394+a-maci29@users.noreply.github.com>
2021-10-12 22:58:42 -0400

    answer
    Random name/Initials generator

---
+++ main.js
@@ -0,0 +1,17 @@
+
+document.querySelector('#generateName').onclick = getRandomName
+
+function getRandomName (){ 
+  fetch('https://www.behindthename.com/api/random.json?usage=ita&gender=f&key=sa542120201') 
+    .then (response => response.json()) 
+    .then (data => { 
+      document.querySelector('#first').innerHTML = data.names[0]
+      document.querySelector('#last').innerHTML = data.names[1]
+      let newFirst = data.names[0]
+      let newLast= data.names[1]
+      document.querySelector('#img').src = `https://ui-avatars.com/api/?name=${newFirst}+${newLast}` 
+
+      // https://ui-avatars.com/api/?background=random
+  }
+  )
+} 
