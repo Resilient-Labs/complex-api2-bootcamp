68b9356f8eb0e5cb1fa9c41c1dbe6f608d43a46a
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-07 23:42:07 -0400

    update readme

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Moon Phase API
+# Cat Facts
 
-###
+### Goal
+Show cat facts and search wikipedia for what a cat is for those who might not be aware.
 
 ### How It's Made
-
+I used HTML and CSS+Flexbox to style, structure and position.
 
 ### Functionality
+For functionality I used Javascript to retrieve cat facts from a random cat facts API generator. I then took those cat facts and used it's data to search wikipedia for what a cat is.
--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 //Retreive data from random date generator and then take that date and retreive moon phase image
 
-document.querySelector("button").addEventListener("click", search)
+document.querySelector("button").addEventListener("click", search);
 
 function search() {
-  const catFactsUrl = 'https://cat-fact.herokuapp.com/facts'
-  let wikiCat ;
-
-
+  const catFactsUrl = "https://cat-fact.herokuapp.com/facts";
+  let wikiCat;
 
   fetch(catFactsUrl)
-      .then(res =>{
-        responseCode = res.status
-        return res.json()
-      }  ) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        for (let i = 0; i < data.length; i++) {
-          let section = document.querySelector("section");
-          const p = document.createElement("p");
-          section.appendChild(p);
-          p.innerText = data[i].text
-          wikiCat = data[i].type
-        }
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        let section = document.querySelector("section");
+        const p = document.createElement("p");
+        section.appendChild(p);
+        p.innerText = data[i].text;
+        wikiCat = data[i].type;
+      }
 
-        const wikiUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${wikiCat}&format=json&origin=*`;
+      const wikiUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${wikiCat}&format=json&origin=*`;
 
-        fetch(wikiUrl)
-          .then((res) => res.json()) // parse response as JSON
-          .then((wikiData) => {
-            document.querySelector("h5").innerText =
-              wikiData.query.search[0].snippet;
+      fetch(wikiUrl)
+        .then((res) => res.json()) // parse response as JSON
+        .then((wikiData) => {
+          document.querySelector("h5").innerText =
+            wikiData.query.search[0].snippet;
 
-              document.getElementById('what').innerText = "What is a cat?"
-              document.getElementById('facts').innerText = "Here are some fun facts!!!"
-
-          })
-          .catch((err) => {
-            console.log(`error ${err}`);
-          });
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
- });
- }
+          document.getElementById("what").innerText = "What is a cat?";
+          document.getElementById("facts").innerText =
+            "Here are some fun facts!!!";
+        })
+        .catch((err) => {
+          console.log(`error ${err}`);
+        });
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
