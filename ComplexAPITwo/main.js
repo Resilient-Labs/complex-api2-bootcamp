56a73b357f849f5ca8608f66e29c6278ecb0a15a
Sarah Bouayad <112355619+sarahbouayad@users.noreply.github.com>
2022-10-18 00:20:05 -0400

    Update main.js

--- ComplexAPITwo/main.js
+++ ComplexAPITwo/main.js
@@ -7,25 +7,25 @@ function findTheRecipe() {
     .then((res) => res.json())
     .then((data) => {
 
-      for (let i = 0; i < data.length; i++) {
+      for (let i = 0; i < data.meals.length; i++) {
         let theInstructions = document.getElementById('instructions')
         let theMealName = document.getElementById('mealName')
-
+        
         theMealName.innerText = `${data.meals[i].strMeal}`
         theInstructions.innerText = `This is recipe: ${data.meals[i].strInstructions}`;
+      
+    
+        let secondUrl = `https://api.unsplash.com/search/photos?query=${data.meals[i].strTags}&client_id=_baZTx44MDM2d9AUZAsaUqbp6_SlevRviuHi2GUWkzQ`;
        
-
-        let secondUrl = `https://api.spoonacular.com/recipes/random?apiKey=c2c8fa39ba3c4ec491ea56bb5c949fca`;
-        let randomRecipes = document.querySelector("h3");
         
-        console.log(data)
+      //  api to populate recipe photo
 
         fetch(secondUrl)
           .then((res) => res.json())
           .then((data2) => {
-            console.log(data2);
-            randomRecipes.innerText = `${data2}`;
-            console.log(data);
+            // debugger
+            // console.log(data2)
+            document.querySelector("img").src = data2.results[i].urls.thumb
           });
       }
     })
@@ -36,5 +36,3 @@ function findTheRecipe() {
       console.log(`err ${err}`);
     });
 }
-
-findTheRecipe();
