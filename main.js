ca09811bb9afb11054a8f2a622af74dbbbc95624
bownejp20 <jbowne24@gmail.com>
2023-04-18 15:17:02 -0400

    fixed my js error of not common names not coming up like the name vonds and changed my html name from idex to index

--- main.js
+++ main.js
@@ -11,18 +11,28 @@ function numberToFact() {
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      console.log(`first fetch reslt = ${data}`)
-      nameH2.innerText = data.name
-      ageH2.innerText = data.age
-      let nameAge = data.age
+      if(data.name === undefined){
+        nameH2.innerText = undefined
+        ageH2.innerText = undefined
+      }else {
+        console.log(`first fetch reslt = ${data}`)
+        nameH2.innerText = data.name
+        ageH2.innerText = data.age
+      }
+      let nameAge = data.age 
 
       const url2 = `http://numbersapi.com/${nameAge}`
       console.log(`url2 = ${url2}`)
       fetch(url2)
         .then(res => res.text())
         .then(factData => {
-          console.log(`fetch 2 result = ${factData}`)
-          factH2.innerText = factData// need
+          if(nameAge === null){
+            factH2.innerText = `Sorry,undetected name, you'll live forever!`
+          }else{
+            console.log(`fetch 2 result = ${factData}`)
+            factH2.innerText = factData
+          }
+      
         })
      }
     )
