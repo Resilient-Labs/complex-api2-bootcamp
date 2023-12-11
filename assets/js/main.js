c65e7a98edf43822fce865a78827ac575837c7a5
socratic617 <erik.solis02135@gmail.com>
2023-12-11 14:35:46 -0500

    cleaning up project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,10 @@
 //The user will enter a a country or city. Use that location to get the weather of that day! https://openweathermap.org/api
 
-//connect button  function "getTemp"
 document.querySelector("button").addEventListener("click", getQR);
 
 function getQR() {
   //getting my users random photo
   const newPic = document.querySelector("#newPhoto");
-
 
   // have to print what you are trying to test
   console.log("new Pic: " + newPic);
@@ -14,7 +12,6 @@ function getQR() {
 
   const url = `https://api.pexels.com/v1/search?query=${inputText}`
   const options = {
-    //POST method is used when you are trying to recieve certain info from the server
     method: "GET",
     headers: {
       "Authorization": "xMznuEQB2Bdv4Y4e5rqT4URbzhRp5h2coATn2e6VILVEE1OIz4XIu9J5",
@@ -22,7 +19,7 @@ function getQR() {
 
   };
 
-  //using method fetch to request my data from that api using url
+  //using method fetch to request my data from api using url
   fetch(url, options) // go get api
     //.then property passes param res which responds with JSON parsing out the data you requested
     .then((res) => res.json())
@@ -38,21 +35,19 @@ function getQR() {
         "Here is the QR Code for the photo!";
 
 
-      //
-
       let inputQR = data.photos[0].url
       const urlQR = `http://api.qrserver.com/v1/create-qr-code/?data=${inputQR}&size=200x200`
 
       fetch(urlQR, {method: 'GET'})
-        // .then((res) => res.json())
         .then((data) => {
           console.log("data:");
           console.log(data);
           console.log("data.photos:")
+
           // check to see if the data url filtered into the api
           console.log(data.url)
-          document.querySelector("#photoQR").src = data.url;
 
+          document.querySelector("#photoQR").src = data.url;
           document.querySelector("#hidden").id = "";
 
         })
@@ -63,5 +58,4 @@ function getQR() {
     .catch((err) => {
       console.log(`error ${err}`);
     });
-   
 }
